b5edf618a45a50e2bc361046093ebdf5ee808378
Sam Thanawalla <samir.thanawalla@gmail.com>
2020-07-08 17:40:00 +0000

    added working configuration features

--- example/usps_api/config/config_parser.h
+++ example/usps_api/config/config_parser.h
@@ -11,11 +11,29 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations under
 // the License.
+#include "proto/usps_api/sfc_filter.pb.h"
 #include "json/json.h"
+#include <string>
+#include <list>
+
 namespace usps_api_server {
   class Config {
     public:
+      struct Filter {
+        std::list<ghost::GhostTunnelIdentifier> tunnels;
+        std::list<ghost::GhostRoutingIdentifier> routings;
+      };
+      std::string host;
+      std::uint16_t port;
+      bool create;
+      bool del;
+      bool query;
+      int delay_time;
+      Filter deny, allow, delay;
       void ParseConfig(Json::Value root);
       void Initialize();
+      void ParseIdentifiers(Filter* filter, Json::Value root);
+      bool FilterMatch(Filter* filter, ghost::SfcFilter sfc_filter);
+      bool FilterActive(Filter* filter);
   };
 }
